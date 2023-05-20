89d1e078f2dd272f0378b2ab64d29468c2a3c932
fabricioFalcao <fabriciohfd@gmail.com>
2023-05-20 19:30:00 -0300

    Requisitos 2 e 3

--- script.js
+++ script.js
@@ -16,8 +16,21 @@ function elementCreator(elementType, elementId, elementClass, elementText) {
 
 // 1 - Adicione à página o título "Paleta de Cores"
 
-document.body.appendChild(elementCreator('h1', 'title', 0, 'Paleta de Cores'));
+const header = document.querySelector('header');
+header.appendChild(elementCreator('h1', 'title', 0, 'Paleta de Cores'));
 
-// elementCreator('h1', 'title', null, 'Paleta de cores');
-// document.body.appendChild(element)
+// 2 - Adicione à página uma paleta contendo quatro cores distintas
 
+const nav = document.querySelector('nav');
+nav.appendChild(elementCreator('section', 'color-palette', 0, 0));
+
+const colorPallete = (colors) => {
+  const pallete = document.querySelector('#color-palette');
+  for (let i = 0; i < colors.length; i += 1) {
+    const color = colors[i];
+    const currentColor = elementCreator('div', 0, 'color', 0);
+    currentColor.style.backgroundColor = color;
+    pallete.appendChild(currentColor);
+  }
+};
+colorPallete(['black', 'red', 'yellow', 'green']);
