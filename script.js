aa6234871e98af46dacf0cf147100ea5dba62325
fabricioFalcao <fabriciohfd@gmail.com>
2023-05-21 17:36:59 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -28,11 +28,12 @@ const colorPallete = (colors) => {
   const pallete = document.querySelector('#color-palette');
   for (let i = 0; i < colors.length; i += 1) {
     const color = colors[i];
-    const currentColor = elementCreator('div', i, 'color', 0);
+    const currentColor = elementCreator('div', 0, 'color', 0);
     currentColor.style.backgroundColor = color;
     pallete.appendChild(currentColor);
   }
 };
+colorPallete(['black', 'red', 'yellow', 'green']);
 
 // 4 - Adicione um botão para gerar cores aleatórias para a paleta de cores
 
@@ -79,10 +80,14 @@ for (let i = 1; i <= 25; i += 1) {
   board.appendChild(elementCreator('div', 0, 'pixel', 0));
 };
 
+// 8 - Defina a cor preta como cor inicial da paleta de cores
+
+const blackBox = document.querySelector('.color');
+blackBox.classList.add('selected');
+
 // Functions on load
 
 window.onload = () => {
-  colorPallete(['black', 'red', 'yellow', 'green']);
   loadPallete();
 
 };
